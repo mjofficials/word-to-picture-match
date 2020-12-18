dfe4721e766a039b7e3621494675db4685e2be26
mjofficials <manojmarimuthu1995@gmail.com>
2020-12-18 23:03:28 +0530

    tested color change function

--- new_script.js
+++ new_script.js
@@ -1,6 +1,9 @@
 // Testing if-else 
 const tomato = document.getElementById('inputA');
-
+const iceCream = document.getElementById('inputB');
+const chicken = document.getElementById('inputC');
+const lion = document.getElementById('inputD');
+const car = document.getElementById('inputE');
 // tomato.addEventListener('change', (e) => {
 //     (e.target.checked || !(tomato.classList.contains('classAtoImage'))) ? (console.log('Tomato ON'), tomato.classList.add('classAtoImage'))
 //         : ((tomato.classList.remove('classAtoImage'), console.log('Tomato OFF')));
@@ -8,23 +11,63 @@ const tomato = document.getElementById('inputA');
 
 var testArr = [];
 
+// tomato
 tomato.addEventListener('change', (e) => {
     if (e.target.checked && !(tomato.classList.contains('classAtoImage'))) {
-        foundOrnot();
         testArr.push('classAtoImage');
         console.log(testArr);
     } else {
-        foundOrnot();
         testArr = testArr.filter(e => e !== 'classAtoImage');
         console.log(testArr);
     }
 });
 
-
-function foundOrnot() {
-    if ((testArr.indexOf('classAtoImage') != -1)) {
-        console.log('not found');
+// icecream
+iceCream.addEventListener('change', (e) => {
+    if (e.target.checked && !(iceCream.classList.contains('classBtoImage'))) {
+        testArr.push('classBtoImage');
+        console.log(testArr);
     } else {
-        console.log(' found')
+        testArr = testArr.filter(e => e !== 'classBtoImage');
+        console.log(testArr);
     }
+});
+
+// chicken
+chicken.addEventListener('change', (e) => {
+    if (e.target.checked && !(iceCream.classList.contains('classCtoImage'))) {
+        testArr.push('classCtoImage');
+        console.log(testArr);
+    } else {
+        testArr = testArr.filter(e => e !== 'classCtoImage');
+        console.log(testArr);
+    }
+});
+
+// lion
+lion.addEventListener('change', (e) => {
+    if (e.target.checked && !(iceCream.classList.contains('classDtoImage'))) {
+        testArr.push('classDtoImage');
+        console.log(testArr);
+    } else {
+        testArr = testArr.filter(e => e !== 'classDtoImage');
+        console.log(testArr);
+    }
+});
+// car
+car.addEventListener('change', (e) => {
+    if (e.target.checked && !(iceCream.classList.contains('classEtoImage'))) {
+        testArr.push('classEtoImage');
+        console.log(testArr);
+    } else {
+        testArr = testArr.filter(e => e !== 'classEtoImage');
+        console.log(testArr);
+    }
+});
+
+// color change test
+
+function changeColor() {
+    const change24 = document.styleSheets[0].cssRules[24].style;
+    change24.setProperty('background', 'red');
 }
